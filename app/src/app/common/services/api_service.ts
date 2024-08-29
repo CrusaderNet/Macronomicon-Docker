7594daf7ffab11fe6790e6858f885e260123e717
CrusaderNet <110443549+CrusaderNet@users.noreply.github.com>
2024-08-29 03:10:19 -0500

    fixed DB access, must still edit to allow external access

--- app/src/app/common/services/api_service.ts
+++ app/src/app/common/services/api_service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from "@angular/core";
-
-import {type MacroEntry} from "../models/macro_entry";
-import {type User} from "../models/user";
+import { MacroEntry } from "../models/macro_entry";
+import { User } from "../models/user";
 
 @Injectable()
 export class ApiService {
     constructor() {}
 
     listMacroEntries(userID: number): Promise<MacroEntry[]> {
-        return fetch(`http://localhost:8080/macro_entries?userID=${userID}`).then(response => response.json() as Promise<MacroEntry[]>)
+        return fetch(`http://macronomicon-docker-db-1/macro_entries?userID=${userID}`).then(response => response.json() as Promise<MacroEntry[]>);
     }
 
     listUsers(): Promise<User[]> {
-        return fetch('http://localhost:8080/users').then(response => response.json() as Promise<User[]>)
+        return fetch('http://macronomicon-docker-db-1:8080/users').then(response => response.json() as Promise<User[]>);
     }
 }
