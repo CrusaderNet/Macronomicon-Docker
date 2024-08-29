91aa4dce5983e5c85edfd8debc21fce9337f7928
CrusaderNet <110443549+CrusaderNet@users.noreply.github.com>
2024-08-29 00:37:47 -0500

    Updated Angular and made changes to the compose

--- api/database/db.go
+++ api/database/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -26,13 +27,15 @@ func Init() error {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local", DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 	fmt.Println("dsn:", dsn)
 
+	println("Connecting to database. . . . . .")
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return fmt.Errorf("error connecting to database. error = %w", err)
 		//os.Exit(1)
 	}
 
-	println("Connected to database")
+	println("Connected to database!")
 
 	DB = db
 	return nil
